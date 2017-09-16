78b76e329e1cb1769e9003150d21130b5e41f89a
John <johnhanbury4@gmail.com>
2017-09-16 01:43:17 +0100

    Added StringToNameConverter Upgraded to .net Standard 2.0 Added Unit tests

---
+++ XamHelper/XamHelper/Converters/StringToNameConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace XamHelper.Converters
+{
+    public class StringToNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var input = value.ToString();
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(input.ToLower());
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString();
+        }
+    }
+}
